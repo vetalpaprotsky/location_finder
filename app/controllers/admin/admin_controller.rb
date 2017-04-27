9d5da7715515133d11bef246f63271ccfe578827
vitalypaprotsky <vetalpaprotsky@gmail.com>
2017-04-28 00:33:02 +0300

    add http basic auth

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,5 +1,9 @@
 module Admin
   class AdminController < ActionController::Base
+    http_basic_authenticate_with(
+      name: ENV['HTTP_BASIC_AUTH_USER'],
+      password: ENV['HTTP_BASIC_AUTH_PASSWORD']
+    )
     protect_from_forgery with: :exception
     layout 'admin'
   end
