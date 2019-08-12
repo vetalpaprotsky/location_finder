fdff00b6c1e122d1e16bd15514c270366b5b0b53
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2019-08-12 17:44:00 +0300

    set correct time zone

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module LocationFinder
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.time_zone = 'Kyiv'
+    config.active_record.default_timezone = :local
   end
 end
